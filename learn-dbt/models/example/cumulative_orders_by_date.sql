728a1b1f7e129a2032e6f26b817337c5aa187399
Darrell Leong <a843183@yara.com>
2022-05-24 18:05:22 +0800

    completed model assignment

---
+++ learn-dbt/models/example/cumulative_orders_by_date.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+
+with daily_sales_table as(
+  select
+      o_orderdate date,
+      sum(o_totalprice) daily_sales
+  from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS"
+
+  group by date
+
+  order by date asc)
+
+select
+    date,
+    sum(daily_sales) over (order by date asc) cum_sales
+from daily_sales_table
